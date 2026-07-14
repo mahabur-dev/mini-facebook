f9764a091fc1240aa861a0ce313b1e995e32ff7e
Mahabur Rahman <121110573+mahabur-dev@users.noreply.github.com>
2026-07-14 14:58:37 +0600

    Add new CORS origins for deployment

--- server/src/main.ts
+++ server/src/main.ts
@@ -330,6 +330,9 @@ function createCorsOriginValidator(configService: ConfigService, port: number) {
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
+    "https://mini-facebook-ochre.vercel.app",
+    "https://mini-facebook-git-main-mahabur1814031-8144s-projects.vercel.app",
+    "https://mini-facebook-production.up.railway.app"
     `http://localhost:${port}`,
     ...configuredOrigins,
   ]);
